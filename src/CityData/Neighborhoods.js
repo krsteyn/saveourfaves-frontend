7c19ed439553f25e073e6bde9ec0d254c7de15c5
Kornel Steyn <kornel@zailab.com>
2020-04-05 04:52:55 +0200

    update neighbourhood for each area

--- src/CityData/Neighborhoods.js
+++ src/CityData/Neighborhoods.js
@@ -1,6 +1,6 @@
 const Neighborhoods = {
   CPT: [{ name: "CBD", key: "CBD", image: null }],
-  JHB: [],
-  DUR: []
+  JHB: [{ name: "CBD", key: "CBD", image: null }],
+  DUR: [{ name: "CBD", key: "CBD", image: null }]
 };
 export default Neighborhoods;
