e172a8ad5a59f80cbe594569729118c020565040
Kornel Steyn <kornel@zailab.com>
2020-04-06 21:59:36 +0200

    header logo size

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ class App extends React.Component {
                 <Row className="top-header" justify="center">
                   <Col span={24} offset={0}>
                     <Title style={{ float: "left", color: "white" }} level={4}>
-                      <img src="/landscape-logo.png" />
+                      <img src="/landscape-logo.png" className="header-logo" />
                     </Title>
                     <div style={{ float: "right" }}>
                       <a href="#">
