42158f48760d273cff044b0e6523e8498b0f5bee
Kornel Steyn <kornel@zailab.com>
2020-04-08 23:01:48 +0200

    try and fix responsive behaviour

--- src/App.js
+++ src/App.js
@@ -130,127 +130,144 @@ class App extends React.Component {
               }}
             />
             <Row className="hero-row">
-              <Row className="centre-box">
-                <Row className="top-header" justify="center">
-                  <Col span={24} offset={0}>
-                    <Title style={{ float: "left", color: "white" }} level={4}>
-                      <img src="/landscape-logo.png" className="header-logo" />
-                    </Title>
-                    <div style={{ float: "right" }}>
-                      <a href="#">
-                        <Title
-                          onClick={() => {
-                            this.showFAQModal();
-                          }}
-                          className="header-link"
-                          level={4}
-                        >
-                          FAQ
-                        </Title>
-                      </a>
-                      <Popover content={<ShareOptions />}>
-                        <a>
+              {/*<Row className="centre-box">*/}
+              <Row>
+                <Col
+                  offset={5}
+                  span={14}
+                  xs={{ span: 24, offset: 0 }}
+                  sm={{ span: 24, offset: 0 }}
+                  md={{ span: 18, offset: 3 }}
+                >
+                  <Row className="top-header" justify="center">
+                    <Col span={24} offset={0}>
+                      <Title
+                        style={{ float: "left", color: "white" }}
+                        level={4}
+                      >
+                        <img
+                          src="/landscape-logo.png"
+                          className="header-logo"
+                        />
+                      </Title>
+                      <div style={{ float: "right" }}>
+                        <a href="#">
                           <Title
-                            className="header-link header-share-link"
+                            onClick={() => {
+                              this.showFAQModal();
+                            }}
+                            className="header-link"
                             level={4}
                           >
-                            Tell friends
+                            FAQ
                           </Title>
                         </a>
-                      </Popover>
-                      <Button
-                        onClick={event => {
-                          this.showAddModal();
-                        }}
-                        shape="round"
-                        className="header-button header-add-place-button"
-                      >
-                        Add a Place
-                      </Button>
-                    </div>
-                  </Col>
-                </Row>
+                        <Popover content={<ShareOptions />}>
+                          <a>
+                            <Title
+                              className="header-link header-share-link"
+                              level={4}
+                            >
+                              Tell friends
+                            </Title>
+                          </a>
+                        </Popover>
+                        <Button
+                          onClick={event => {
+                            this.showAddModal();
+                          }}
+                          shape="round"
+                          className="header-button header-add-place-button"
+                        >
+                          Add a Place
+                        </Button>
+                      </div>
+                    </Col>
+                  </Row>
 
-                <Col
-                  xs={{ span: 18, offset: 3 }}
-                  span={16}
-                  offset={4}
-                  style={{ textAlign: "center", padding: "20px 0px" }}
-                >
-                  <Title
-                    level={1}
-                    style={{ color: "white", textAlign: "center" }}
-                  >
-                    Your favorite South African small business might close
-                    forever. Help save it.
-                  </Title>
-                </Col>
-                <Col
-                  sm={{ span: 20, offset: 2 }}
-                  md={{ span: 20, offset: 2 }}
-                  lg={{ span: 20, offset: 2 }}
-                >
-                  <div className="main-results">
-                    <div style={{ padding: 20 }}>
-                      <AreaContext.Consumer>
-                        {value => {
-                          return (
-                            <PlaceFilterDisplay updateArea={value.updateArea} />
-                          );
-                        }}
-                      </AreaContext.Consumer>
-                    </div>
-                    <div className="neighborhood-card-container-outer">
-                      <AreaContext.Consumer>
-                        {value => {
-                          return (
-                            <NeighborhoodCards
-                              currentArea={value.currentArea}
-                              updateArea={value.updateArea}
-                            />
-                          );
-                        }}
-                      </AreaContext.Consumer>
-                    </div>
-                  </div>
-                </Col>
-                <Row className="footer-content">
                   <Col
-                    offset={2}
-                    xs={18}
-                    sm={18}
-                    md={9}
-                    lg={9}
-                    xl={9}
-                    style={{ textAlign: "left" }}
+                    xs={{ span: 18, offset: 3 }}
+                    span={16}
+                    offset={4}
+                    style={{ textAlign: "center", padding: "20px 0px" }}
                   >
-                    <Title level={3}>Our duty as loyal customers</Title>
-                    <p>
-                      Our small businesses need us more than ever. Even though
-                      we can't leave home, we can still support our local
-                      restaurants, and other small businesses by buying a
-                      voucher now, use it later.
-                    </p>
+                    <Title
+                      level={1}
+                      style={{ color: "white", textAlign: "center" }}
+                    >
+                      Your favorite South African small business might close
+                      forever. Help save it.
+                    </Title>
                   </Col>
                   <Col
-                    offset={2}
-                    xs={18}
-                    sm={18}
-                    md={9}
-                    lg={9}
-                    xl={9}
-                    style={{ textAlign: "left" }}
+                    sm={{ span: 20, offset: 2 }}
+                    md={{ span: 20, offset: 2 }}
+                    lg={{ span: 20, offset: 2 }}
                   >
-                    <Title level={3}>3 weeks can kill a business</Title>
-                    <p>
-                      Businesses have many fixed costs: rent, labour, loan
-                      repayments, insurance, supplies, repairs - and the list
-                      goes on. Even successful small businesses have very thin
-                      margins of 3-5%. The lockdown is keeping customers at
-                      home, and tipping the balance towards bankruptcy.
-                    </p>
+                    <div className="main-results">
+                      <div style={{ padding: 20 }}>
+                        <AreaContext.Consumer>
+                          {value => {
+                            return (
+                              <PlaceFilterDisplay
+                                updateArea={value.updateArea}
+                              />
+                            );
+                          }}
+                        </AreaContext.Consumer>
+                      </div>
+                      <div className="neighborhood-card-container-outer">
+                        <AreaContext.Consumer>
+                          {value => {
+                            return (
+                              <NeighborhoodCards
+                                currentArea={value.currentArea}
+                                updateArea={value.updateArea}
+                              />
+                            );
+                          }}
+                        </AreaContext.Consumer>
+                      </div>
+                    </div>
                   </Col>
-                </Row>
+                  <Row className="footer-content">
+                    <Col
+                      offset={2}
+                      xs={18}
+                      sm={18}
+                      md={9}
+                      lg={9}
+                      xl={9}
+                      style={{ textAlign: "left" }}
+                    >
+                      <Title level={3}>Our duty as loyal customers</Title>
+                      <p>
+                        Our small businesses need us more than ever. Even though
+                        we can't leave home, we can still support our local
+                        restaurants, and other small businesses by buying a
+                        voucher now, use it later.
+                      </p>
+                    </Col>
+                    <Col
+                      offset={2}
+                      xs={18}
+                      sm={18}
+                      md={9}
+                      lg={9}
+                      xl={9}
+                      style={{ textAlign: "left" }}
+                    >
+                      <Title level={3}>3 weeks can kill a business</Title>
+                      <p>
+                        Businesses have many fixed costs: rent, labour, loan
+                        repayments, insurance, supplies, repairs - and the list
+                        goes on. Even successful small businesses have very thin
+                        margins of 3-5%. The lockdown is keeping customers at
+                        home, and tipping the balance towards bankruptcy.
+                      </p>
+                    </Col>
+                  </Row>
+                </Col>
               </Row>
               <Row className="footer-menu">
                 <Col
